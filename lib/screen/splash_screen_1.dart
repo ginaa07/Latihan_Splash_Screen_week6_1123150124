fe36b9ba165407c36b6a607fbbe76c4d16196bb6
ginaa07 <1123150124@global.ac.id>
2025-11-01 02:06:00 +0700

    create page splash screen scaffold awal

---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,12 @@
+// ini adalah contoh code pada saat di generate nama class kalian rubah sesuai
+
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
