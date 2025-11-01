cf2f569399d280906ae2fb51f777f6c407a2ae90
ginaa07 <1123150124@global.ac.id>
2025-11-01 21:09:01 +0700

    Penambahan focusedBorder dan colors untuk password

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -60,7 +60,10 @@ class Login extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(color: Colors.green),
                 ),
-
+                focusedBorder: OutlineInputBorder(
+                borderRadius:  BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.blue, width: 2),
+                )
               ),
               ),
               )
