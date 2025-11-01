5ea93ad5c88fa07b979d3102a9755604ed7bb751
ginaa07 <1123150124@global.ac.id>
2025-11-01 18:41:58 +0700

    melakukan perubahan onPress pada ElevatButton splash screen 2 dan ganti image

--- lib/screen/splash_screen_2.dart
+++ lib/screen/splash_screen_2.dart
@@ -41,7 +41,7 @@ class SplashScreen2 extends StatelessWidget {
         ),
         SizedBox(height: 20),
         Text(
-          "Hallo, Selamat datang di aplikasi saya!",
+          "Hallo, welcome to myApp!",
           style: TextStyle(
             fontSize: 20,
             color: Colors.grey[700],
